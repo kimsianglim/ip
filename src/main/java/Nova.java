909e0ed599163368e018d4f1860f129905374ae1
Lim Kim Siang <limkimsiang05@gmail.com>
2026-01-27 07:18:01 +0800

    Rename

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,6 +1,6 @@
-public class KS {
+public class Nova {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm KS");
+        System.out.println("Hello! I'm Nova");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
