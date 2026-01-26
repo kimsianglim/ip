8c11d67d5f9c9b99574cb4bf4ecc1288dbb9c751
Lim Kim Siang <limkimsiang05@gmail.com>
2026-01-27 07:28:41 +0800

    Level-1

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Nova");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("----------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("----------------------------------------");
+                break;
+            }
+
+            System.out.println("----------------------------------------");
+            System.out.println(input);
+            System.out.println("----------------------------------------");
+        }
     }
 }
