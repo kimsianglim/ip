dd50af8fea742d4d3ef275df9e9e2121fe633ac9
Lim Kim Siang <limkimsiang05@gmail.com>
2026-02-13 12:51:38 +0800

    debug TaskList

--- src/main/java/task/TaskList.java
+++ src/main/java/task/TaskList.java
@@ -96,4 +96,15 @@ public class TaskList {
     public ArrayList<Task> getTasks() {
         return new ArrayList<>(tasks);
     }
+
+    public ArrayList<Integer> findIndexes(String keyword) {
+        ArrayList<Integer> matches = new ArrayList<>();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getDescription().contains(keyword)) {
+                matches.add(i);
+            }
+        }
+        return matches;
+    }
+
 }
